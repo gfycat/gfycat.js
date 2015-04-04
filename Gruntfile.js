421bff421ff171c4e0b78143645d2bfea55db5a0
sarahjsquire <sarahjsquire@gmail.com>
2015-04-04 13:09:25 -0400

    Push on publish

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,8 +41,7 @@ module.exports = function(grunt) {
     },
     'gh-pages': {
       options: {
-        base: 'demo',
-        push: false
+        base: 'demo'
       },
       src: '**/*'
     }
