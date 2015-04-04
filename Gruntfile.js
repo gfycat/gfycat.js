7a5ea6f9efa0a24dfbf0af02c06b3c073b7ba29e
sarahjsquire <sarahjsquire@gmail.com>
2015-04-04 13:01:52 -0400

    Add build, github pages and Gruntfile to run it all

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/*.js'],
+        dest: 'build/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    copy: {
+      main: {
+        src: 'build/*',
+        dest: 'demo/',
+      },
+    },
+    'gh-pages': {
+      options: {
+        base: 'demo',
+        push: false
+      },
+      src: '**/*'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
+
+  grunt.registerTask('default', ['build']);
+
+  grunt.registerTask('demo', ['build', 'copy']);
+
+  grunt.registerTask('publish', ['demo', 'gh-pages']);
+
+};
